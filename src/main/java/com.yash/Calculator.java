d2b55ba4c4353bad0dc6658876cf7d5871d8608e
Yash Barot <Yash2506.yb.yb@gmail.com>
2021-05-17 09:56:55 -0400

    files added

---
+++ src/main/java/com.yash/Calculator.java
@@ -0,0 +1,12 @@
+package com.yash;
+
+public class Calculator {
+
+    public  int multiplier(int x) {
+
+
+        return x * 2;
+
+    }
+
+}
---
+++ src/main/java/com.yash/Test.java
@@ -0,0 +1,17 @@
+package com.yash;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.multiplier(4));
+    }
+
+
+
+
+}
+
